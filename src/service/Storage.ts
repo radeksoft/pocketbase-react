ff92c18b2b6e832804503b346d36e86a4b437f51
Marian Šámal <margotka000@gmail.com>
2023-06-03 20:20:36 +0200

    Remove React Native

--- src/service/Storage.ts
+++ src/service/Storage.ts
@@ -1,5 +1,3 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
 export class StorageService {
   static Constants = {
     SUBSCRIBED: 'subscribed',
@@ -7,24 +5,14 @@ export class StorageService {
   };
 
   static async get(key: string): Promise<string | null> {
-    return typeof document !== 'undefined'
-      ? localStorage.getItem(key)
-      : await AsyncStorage.getItem(key);
+    return localStorage.getItem(key)
   }
 
   static async set(key: string, value: string): Promise<void> {
-    if (typeof document !== 'undefined') {
-      return localStorage.setItem(key, value);
-    } else {
-      return await AsyncStorage.setItem(key, value);
-    }
+    return localStorage.setItem(key, value);
   }
 
   static async remove(key: string): Promise<void> {
-    if (typeof document !== 'undefined') {
-      return localStorage.removeItem(key);
-    } else {
-      return await AsyncStorage.removeItem(key);
-    }
+    return localStorage.removeItem(key);
   }
 }
